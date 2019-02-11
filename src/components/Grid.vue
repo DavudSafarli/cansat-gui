e902689151435086414de8984fa5dfda54fe3268
unknown <davudseferli@gmail.com>
2019-02-11 14:14:21 +0400

    aybilet

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -16,6 +16,15 @@ export default {
 };
 </script>
 
-<style lang="scss">
-
+<style lang="scss" scoped>
+.graphs-container{
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  .canvas-wrap{
+    width: 100%;
+    height: 33vh;
+  }
+}
 </style>
